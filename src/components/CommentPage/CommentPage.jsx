9d757599ce4aff285d4ffcb3be2a10f8f47c8f17
Anthony Moe <anthony.moe7@gmail.com>
2021-04-20 18:15:01 -0500

    comment page off to a good start need to create table and post route with mood and comments

--- src/components/CommentPage/CommentPage.jsx
+++ src/components/CommentPage/CommentPage.jsx
@@ -3,15 +3,29 @@ import {useHistory} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 
 function CommentPage() {
-        //renaming functions to make easier to call
-        const history = useHistory();
-        const dispatch = useDispatch();
-        const test = useSelector( (store) =>{
-            return store;
-        });
+    //renaming functions to make easier to call
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const test = useSelector( (store) =>{
+        return store;
+    });
+
+    const [ comment, setComment ] = useState('')
+
+    let handleChange = (event) => {
+        setComment(event.target.value);
+    }
+
+    let submitEntry = () => {
+        console.log(comment);
+    }
+    
 
     return(
         <>
+            <textarea onChange={handleChange} placeholder="comments" name="comments" id="" cols="50" rows="10" type="text"></textarea>
+            <button>Back</button>
+            <button onClick={submitEntry}>Submit</button>
             <p>{JSON.stringify(test)}</p>
         </>
     )
