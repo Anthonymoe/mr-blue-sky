78fbcadd3ac688e3336e80880e19bfdf56701afb
Anthony Moe <anthony.moe7@gmail.com>
2021-05-03 15:21:52 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,19 @@
+--db name mr-blue-sky
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+
+--user table
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+--entry table
+CREATE TABLE "entry" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT, 
+	"mood" INT,
+	"weather" INT,
+	"comment" VARCHAR (1000),
+	"date" VARCHAR (9)
 );
